9c9cffa2580d124a6909d1424c64038fd236893f
Mark Avdeev <printmoonges@yandex.com>
2021-10-22 02:57:58 +0700

    fix: atoms sizing

--- src/components/organisms/LoginForm/index.tsx
+++ src/components/organisms/LoginForm/index.tsx
@@ -16,29 +16,35 @@ const LoginForm: React.FC = () => {
 
     return (
         <>
-            <form className={'form'}>
-                <FormInput
-                    name={'login'}
-                    isError={isError}
-                    placeholder={'Input user name'}
-                    type={'text'}
-                    label={'User name'}
-                    errorText={'Something goes wrong'}
-                />
-                <FormInput
-                    name={'password'}
-                    isError={isError}
-                    placeholder={'Input password'}
-                    type={'password'}
-                    label={'Password'}
-                    errorText={'Something goes wrong'}
-                />
-                <Button
-                    buttonText={'Log In'}
-                    onClick={onClick}
-                    onDoubleClick={onDoubleClick}
-                    isDisabled={isDisabled}
-                />
+            <form className={'login-form'}>
+                <div className={'login-form__input'}>
+                    <FormInput
+                        name={'login'}
+                        isError={isError}
+                        placeholder={'Input user name'}
+                        type={'text'}
+                        label={'User name'}
+                        errorText={'Something goes wrong'}
+                    />
+                </div>
+                <div className={'login-form__input'}>
+                    <FormInput
+                        name={'password'}
+                        isError={isError}
+                        placeholder={'Input password'}
+                        type={'password'}
+                        label={'Password'}
+                        errorText={'Something goes wrong'}
+                    />
+                </div>
+                <div className={'login-form__button'}>
+                    <Button
+                        buttonText={'Log In'}
+                        onClick={onClick}
+                        onDoubleClick={onDoubleClick}
+                        isDisabled={isDisabled}
+                    />
+                </div>
             </form>
         </>
     )
