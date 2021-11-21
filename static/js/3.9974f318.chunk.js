5d2f449a1c8dec194886ae31baae6a0e935a5d0c
Mark Avdeev <printmoonges@yandex.com>
2021-11-21 08:09:51 +0700

    Updates

---
+++ static/js/3.9974f318.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonphw-project"]=this["webpackJsonphw-project"]||[]).push([[3],{221:function(t,c,e){"use strict";e.r(c);var n=e(1),s=e(21),o=e(83),u=e(34),a=e(0);c.default=Object(s.a)((function(){return Object(n.useEffect)((function(){u.a.connect()}),[]),Object(a.jsx)(o.a,{})}))}}]);
+//# sourceMappingURL=3.9974f318.chunk.js.map
